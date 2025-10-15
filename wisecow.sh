1a43156dae106f96570151139d5a97bb1d33cb41
Datta Rahegaonkar <dattarahegaonkar09@gmail.com>
2025-10-15 18:42:40 +0000

    Added: some changes to run longer

--- wisecow.sh
+++ wisecow.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
 
 SRVPORT=4499
-RSPFILE=response
-
-rm -f $RSPFILE
-mkfifo $RSPFILE
-
-get_api() {
-	read line
-	echo $line
-}
-
-handleRequest() {
-    # 1) Process the request
-	get_api
-	mod=`fortune`
-
-cat <<EOF > $RSPFILE
-HTTP/1.1 200
-
-
-<pre>`cowsay $mod`</pre>
-EOF
-}
 
 prerequisites() {
 	command -v cowsay >/dev/null 2>&1 &&
@@ -33,12 +11,30 @@ prerequisites() {
 		}
 }
 
+handle_request() {
+    while read line; do
+        if [[ "$line" == $'\r' ]] || [[ -z "$line" ]]; then
+            break
+        fi
+    done
+    
+    local fortune_text=$(fortune)
+    local cowsay_output=$(cowsay "$fortune_text")
+    
+    printf "HTTP/1.1 200 OK\r\n"
+    printf "Content-Type: text/html\r\n"
+    printf "Content-Length: %d\r\n" $((${#cowsay_output} + 46))
+    printf "Connection: close\r\n"
+    printf "\r\n"
+    printf "<html><body><pre>%s</pre></body></html>" "$cowsay_output"
+}
+
 main() {
 	prerequisites
 	echo "Wisdom served on port=$SRVPORT..."
 
-	while [ 1 ]; do
-		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+	while true; do
+		handle_request | nc -l -p $SRVPORT
 		sleep 0.01
 	done
 }
